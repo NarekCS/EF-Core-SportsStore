95539e827409d5656b740ffb98ce539ef157137b
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-19 14:38:30 +0400

    Setup

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repo) => repository = repo;
+        public IActionResult Index() => View(repository.Products);
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -15,20 +16,17 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton<IRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
