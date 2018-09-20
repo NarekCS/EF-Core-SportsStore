15fd35a8d02540d42be6a5657e11b6830d684917
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-20 12:39:53 +0400

    CRUD

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -25,5 +25,35 @@ namespace SportsStore.Controllers
             repository.AddProduct(product);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult UpdateProduct(long key)
+        {
+            return View(repository.GetProduct(key));
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            repository.UpdateProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult UpdateAll()
+        {
+            ViewBag.UpdateAll = true;
+            return View(nameof(Index), repository.Products);
+        }
+        [HttpPost]
+        public IActionResult UpdateAll(Product[] products)
+        {
+            repository.UpdateAll(products);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            repository.Delete(product);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- SportsStore/Models/IRepository.cs
+++ SportsStore/Models/IRepository.cs
@@ -8,6 +8,15 @@ namespace SportsStore.Models
     public interface IRepository
     {
         IEnumerable<Product> Products { get; }
+
+        Product GetProduct(long key);
+
         void AddProduct(Product product);
+
+        void UpdateProduct(Product product);
+
+        void UpdateAll(Product[] products);
+
+        void Delete(Product product);
     }
 }
